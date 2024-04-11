97e3e568148e449b77fc6ce7bc051156e7c7072c
Alilou-75 <165791099+Alilou-75@users.noreply.github.com>
2024-04-11 14:55:34 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -18,5 +18,9 @@ def resume_2():
 def resume_template():
     return render_template("resume_template.html")
 
+@app.route('/mon_cv')
+def resume_template():
+    return render_template("mon_cv.html")
+
 if(__name__ == "__main__"):
     app.run()
